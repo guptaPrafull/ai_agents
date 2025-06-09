636b592ca5bea97beda4ff7bba9f1f3220bbddd9
guptaPrafull <createwithai.team@gmail.com>
2025-06-09 17:46:40 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -59,6 +59,3 @@ app.include_router(ocr_router)
 app.include_router(document_insight_router)
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
